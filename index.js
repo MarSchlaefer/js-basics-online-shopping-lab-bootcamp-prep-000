5729deff5adc33b4a3b78c40ff335a0dc822c0cd
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-06-05 15:41:00 +0000

    Done.

--- index.js
+++ index.js
@@ -10,38 +10,58 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var obj = {[item]: Math.floor(Math.random() * 100)}
-  cart.push(obj)
-  
-  console.log(item + " has been added to your cart.")
+  const itemPrice = Math.floor(Math.random() * 100);
+  const itemObj = {[item]: itemPrice};
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart.`)
   return cart;
 }
 
 function viewCart() {
-  var currentCart = "In your cart, you have "
-    
-    if (cart.length === 0) {
-      console.log("Your shopping cart is empty.")
-    }
+  let cartContents = 'In your cart, you have ';
   
-  for (let i = 0; i < cart.length; i++) {
-    let itemObj = cart[i];
-    let itemName = Object.keys(itemObj)[0];
+    if (cart.length === 0) {
+      return console.log("Your shopping cart is empty.")
+    }
     
     if (cart.length === 1) {
-      currentCart = `${currentCart}${itemName} at $${cart[0]}.`
-      
-      console.log(currrentCart);
+      cartContents += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
+      return console.log(cartContents);
     }
-  
-    if (cart.length === 2) {
     
+    if (cart.length === 2) {
+      cartContents += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`;
+      return console.log(cartContents);
     }
-  }
+    
+    else {
+      let cartItems = '';
+      
+      for (let i = 0; i < cart.length - 1; i++) {
+        let currItem = cart[i];
+    
+        if (cart.length >= 3) {
+        
+          cartItems += `${Object.keys(currItem)} at $${currItem[Object.keys(currItem)]}, `
+        }
+      }
+      let lastItem = cart.length - 1;
+      cartContents += `${cartItems} and ${Object.keys(lastItem)} at ${lastItem[Object.keys(lastIem)]}.`
+      
+      return console.log(cartContents);
+  
+    }    
 }
 
 function total() {
-  // write your code here
+  let currentTotal = 0;
+  
+  for (let i = 0; i < cart.length; i++) {
+    let currItem = cart[i];
+    
+    currentTotal += currItem.item;
+  }
+  return currentTotal;
 }
 
 function removeFromCart(item) {
